24c7c4488032d939201cb9a6c6eac52ade8a91d4
Suneet Srivastava <suneetbond91@gmail.com>
2018-01-07 14:02:08 +0530

    Initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="9c25e0b9-4fae-46ff-b420-387bec1c544b")
+public class DataBindingInfo {}
---
+++ app/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="dd765610-b719-4bc1-8b99-81a02be6e440")
+public class DataBindingInfo {}
